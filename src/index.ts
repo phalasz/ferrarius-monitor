ccadf5fc8c132c201c9afd1ee28297982cb03aae
Peter Halasz <skinn3r@gmail.com>
2019-12-06 22:38:13 +0000

    Main entry point

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import path from 'path';
+//import { Server } from 'ferrarius';
+
+const frontendDirectory = path.resolve(__dirname, '..', 'lib', 'static');
+
+export function monitor (server?: any): express.Router {
+  const router = express.Router();
+  router.use('/', express.static(frontendDirectory));
+
+  return router;
+}
