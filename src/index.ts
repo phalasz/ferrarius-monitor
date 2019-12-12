16878665f0a480b609b67ae739348610407b0c2b
Peter Halasz <skinn3r@gmail.com>
2019-12-12 10:26:24 +0000

    remove unneeded paramter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import path from 'path';
-import { Server } from 'ferrarius';
 import { setupApiRoutes } from './api';
 
 const frontendDirectory = path.resolve(__dirname, '..', 'lib', 'static');
 
-export function monitor (server?: Server): express.Router {
+export function monitor (): express.Router {
   const router = express.Router();
   router.use('/', express.static(frontendDirectory));
   router.use('/api', setupApiRoutes());
